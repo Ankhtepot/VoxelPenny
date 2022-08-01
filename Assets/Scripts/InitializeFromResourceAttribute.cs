00d2221f89bddae3b90eeb0977e5f8cf789e5c51
ZavodnyPetr <petr.zavodny@sensearena.com>
2022-08-01 11:06:57 +0200

    InitializeFromResourceAttribute.cs and InitializeFromResourceBase.cs

---
+++ Assets/Scripts/InitializeFromResourceAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class InitializeFromResource : Attribute
+{
+    public readonly string SourcePath;
+    public readonly bool InstantiateImmediately;
+    public Type RequiredPrefabType;
+
+    public InitializeFromResource(string resourcePath = "") : this(resourcePath, typeof(MonoBehaviour), false)
+    {
+    }
+
+    public InitializeFromResource(string resourcePath, Type requiredPrefabType) : this(resourcePath,
+        typeof(MonoBehaviour), false)
+    {
+    }
+
+    public InitializeFromResource(string resourcePath, Type requiredPrefabType, bool instantiateImmediately)
+    {
+        SourcePath = resourcePath;
+        InstantiateImmediately = instantiateImmediately;
+        RequiredPrefabType = requiredPrefabType;
+    }
+}
---
+++ Assets/Scripts/InitializeFromResourceBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+public abstract class InitializeFromResourceBase : MonoBehaviour
+{
+    protected virtual void Awake()
+    {
+        AssignInitializeFromResourceFields();
+    }
+
+    private void AssignInitializeFromResourceFields()
+    {
+        FieldInfo[] fields = GetType().GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+        foreach (FieldInfo field in fields)
+        {
+            if (!(Attribute.GetCustomAttribute(field, typeof(InitializeFromResource), false) is
+                    InitializeFromResource attribute)) continue;
+
+            Debug.Log($"Detected attribute \"{nameof(InitializeFromResource)}\" " +
+                      $"on a field: {field} " +
+                      $"and source path is: {attribute.SourcePath}.");
+
+            GameObject prefab = Resources.Load<GameObject>(attribute.SourcePath);
+
+            if (!prefab)
+            {
+                throw new ArgumentException("Prefab was not loaded on given path.");
+            }
+
+            if (!((attribute.RequiredPrefabType).IsSubclassOf(typeof(Component))))
+            {
+                throw new ArgumentException("Required type on prefab is not inheriting from Component class");
+            }
+
+            if (attribute.RequiredPrefabType != typeof(MonoBehaviour) &&
+                !prefab.GetComponent(attribute.RequiredPrefabType))
+            {
+                throw new ArgumentException("Loaded prefab does not contain required component.");
+            }
+
+            field.SetValue(this, prefab);
+
+            if (!attribute.InstantiateImmediately) continue;
+
+            Instantiate(prefab);
+        }
+    }
+}
